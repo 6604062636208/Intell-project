b7841129abc6613619d1d73cf802a95f69c11144
Your Name <you@example.com>
2025-03-16 13:25:56 +0700

    page of Development Model

--- pages/Demo_Neural_Network.py
+++ pages/Demo_Neural_Network.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import xgboost as xgb
-import lightgbm as lgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
